3f1c6d73d1ad90edd4760a753016d206d7c9b67a
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-03-08 21:48:30 +0700

    Add message, list, header components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import MessageList from './components/MessageList'
+import Header from './components/Header'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        React Firebase Starter
+        <Header title="Message List App"/>
+        <MessageList />
       </div>
     )
   }
